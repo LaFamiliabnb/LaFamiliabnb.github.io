e87b4226ed17f1ef93140d961b2e43e9bc07aaf5
LaFamiliabnb <cjb.fernandez@gmail.com>
2026-07-07 15:55:31 +0200

    Add OVH cron config example

---
+++ ovh/config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'NOWISTAY_API_TOKEN' => 'remplacer_par_le_token_nowistay',
+    'NOWISTAY_API_BASE_URL' => 'https://api.nowistay.com',
+    'SUPABASE_URL' => 'https://ton-projet.supabase.co',
+    'SUPABASE_SERVICE_ROLE_KEY' => 'remplacer_par_la_service_role_key',
+];
